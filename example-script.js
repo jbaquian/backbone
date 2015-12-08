5aea0ccfe0e0877d70b0bb06ec86fe5d808e3aae
jacob <jlb959@uw.edu>
2015-12-07 17:18:43 -0800

    First Example Added

---
+++ example-script.js
@@ -0,0 +1,11 @@
+//Create a variable where you extend the view
+
+	//tell the view where your element should be rendered
+
+	//this creates a template for how our output will be shown
+	template: _.template("<p>Hello my name is <%= who %></p>"),
+	//create an initialization function telling the server to render your inputs
+
+	//create the render function telling where the content will be put
+
+//initialize the view by creating a new view
