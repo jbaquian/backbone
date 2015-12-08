b963e9ccdf30d0c44f1a30352c063e653b052c31
jacob <jlb959@uw.edu>
2015-12-08 00:51:28 -0800

    Second Example Added

--- example-script.js
+++ example-script.js
@@ -1,11 +1,29 @@
+/*First Example*/
+
 //Create a variable where you extend the view
 
-	//tell the view where your element should be rendered
+  //Tell the view where your element should be rendered
 
-	//this creates a template for how our output will be shown
-	template: _.template("<p>Hello my name is <%= who %></p>"),
-	//create an initialization function telling the server to render your inputs
+  //This creates a template for how our output will be shown
+  template: _.template("<p>Hello my name is <%= who %></p>"),
+  //Create an initialization function telling the server to render your inputs
 
-	//create the render function telling where the content will be put
+  //Create the render function telling where the content will be put
+  //To access the template use this.template, don't forget to specfiy who
+  //you are 
 
 //initialize the view by creating a new view
+
+/*Second Example*/
+
+//Create a shared name for everything to reference
+
+//create a model with defaults of a generic first and last name
+
+//Create a new view this time telling the view to render in the other
+//given div and format what is shown to be exactly the same as above
+//Hint to get the first and last names of the model use this.model
+
+//Create a new model for yourself with your first and last name
+
+//Create a new view with the model passed into it
